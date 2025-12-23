e5febf4e9b4611dc963df7c8bf0ad2de5290764a
asvittal17 <asvittal18@gmail.com>
2025-12-23 13:29:33 +0530

    Add responsive portfolio website

---
+++ script.js
@@ -0,0 +1,13 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+});
